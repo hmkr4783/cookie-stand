9d0a98ef38b3df661c87b1554cb2bf4aea1fff2a
Ryankrdh <rhk4783@gmail.com>
2019-02-16 09:44:55 -0800

    rendering of cookie numbers done

--- js/app.js
+++ js/app.js
@@ -21,53 +21,59 @@ var hours = [
 
 var firstPikeUl = document.getElementById('firstpike');
 
-function multiply (a, b) {
-  var result = a*b;
-  var final = Math.floor(result);
-  return (final);
-}
+// function multiply (a, b) {
+//   var result = a*b;
+//   var final = Math.floor(result);
+//   return (final);
+// }
 
-function randomNumber (min, max) {
-  var result = Math.random() * (max - min + 1) + min;
-  var final = Math.floor(result);
-  console.log(final);
-  return final;
-}
-
-
-
-var render = function () {
-  for(var i = 0; i < hours.length; i++) {
-    var liEl = document.createElement('li');
-    
-    var result = Math.floor(Math.random() * (max - min + 1) + min);
-
-
-    firstAndPike.cookiesPerCustomer = multiply(firstAndPike.customersCount, firstAndPike.averageCookies);
-    liEl.textContent = `${hours[i]}: ${firstAndPike.cookiesPerCustomer[i]} cookies.`;
-    firstPikeUl.appendChild(liEl);
-  }
-};
-
+// function randomNumber (min, max) {
+//   var result = Math.random() * (max - min + 1) + min;
+//   var final = Math.floor(result);
+//   console.log(final);
+//   return final;
+// }
 
 var firstAndPike = {
   location: 'First and Pike',
   averageCookies: 6.3,
-  customersCount: randomNumber(23, 65),
   cookiesPerCustomer: 0,
 };
 
+var render = function () {
+  for(var i = 0; i < hours.length; i++) {
+    var liEl = document.createElement('li');
+    var maxCust = 65;
+    var minCust = 23;
+    var customerPerHour = (Math.random() * (maxCust - minCust + 1) + minCust);
+    var cookiesPerCustomer = (customerPerHour*firstAndPike.averageCookies);
+    var final = Math.floor(cookiesPerCustomer);
+    console.log(final);
+    liEl.textContent = `${hours[i]}: ${final} cookies.`;
+    firstPikeUl.appendChild(liEl);
+  }
+};
 
+var firstAndPike = {
+  location: 'First and Pike',
+  averageCookies: 6.3,
+  cookiesPerCustomer: 0,
+};
 
-
-
-
+var render = function () {
+  for(var i = 0; i < hours.length; i++) {
+    var liEl = document.createElement('li');
+    var maxCust = 65;
+    var minCust = 23;
+    var customerPerHour = (Math.random() * (maxCust - minCust + 1) + minCust);
+    var cookiesPerCustomer = (customerPerHour*firstAndPike.averageCookies);
+    var final = Math.floor(cookiesPerCustomer);
+    console.log(final);
+    liEl.textContent = `${hours[i]}: ${final} cookies.`;
+    firstPikeUl.appendChild(liEl);
+  }
+};
 render();
-
-// console.log(firstAndPike.customersCount);
-// console.log(firstAndPike);
-
-// randomNumber(23,53);
 
 
 
